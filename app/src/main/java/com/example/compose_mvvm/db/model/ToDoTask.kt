a58bbd174f75e4c30358b701393314399b7100a9
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2021-09-15 18:36:26 +0900

    Implement Hilt Setup for Room Database

--- app/src/main/java/com/example/compose_mvvm/db/model/ToDoTask.kt
+++ app/src/main/java/com/example/compose_mvvm/db/model/ToDoTask.kt
@@ -1,6 +1,14 @@
-package com.example.compose_mvvm.ui.db.model
+package com.example.compose_mvvm.db.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.compose_mvvm.utility.Constants.DATABASE_TABLE
 
-@Entity
-data class ToDoTask()
+@Entity(tableName = DATABASE_TABLE)
+data class ToDoTask(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    val priority: Priority
+)
--- app/src/main/java/com/example/compose_mvvm/utility/Constants.kt
+++ app/src/main/java/com/example/compose_mvvm/utility/Constants.kt
@@ -1,4 +1,7 @@
 package com.example.compose_mvvm.utility
 
 object Constants {
+
+    const val DATABASE_TABLE = "todo_table"
+    const val DATABASE_NAME = "todo_database"
 }
